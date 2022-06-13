c5f3d332263f176130b518b7ca03181b85b86d55
Sami <68248095+SamiGh-1@users.noreply.github.com>
2022-06-13 09:21:23 +0300

    Add files via upload

---
+++ backend/app.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var app = express();
+app.use(express.json());
+var path = require('path');
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '11923667sS',
+  database : 'users'
+});
+const parser = require('body-parser');
+const urlencodedParser = parser.urlencoded({extended : false});
+
+/* connection.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+}); */
+
+var cors = require("cors")
+app.use(cors({
+  origin:"*"
+}))
+var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+  
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '/fontend/templates/webpage.html'));
+}); 
+app.use(parser .json());
+app.use(urlencodedParser)
+
+app.post('/register',function(req,res){
+  res.send('done');
+  connection.connect(function(err) {
+    console.log("Connected!");  
+    var sql = `INSERT INTO users (name,birthday,address,id,status,wife_id,id_photo,email,net,telephone,add_no,iptv,phone_no,date_added,updated) VALUES (${req.body.name},${req.body.birthday},${req.body.address},${req.body.id},${req.body.status},${req.body.wife_id},${req.body.ID_photo},${req.body.email},${req.body.net},${req.body.telephone},${req.body.add_no},${req.body.iptv},${req.body.phone_no},${date},${req.body.updated})`;
+    connection.query(sql, function (err, result) {
+      console.log("1 record inserted");  
+      }); 
+  }); 
+});
+
+app.put('/update_user',function(req,res){
+  connection.connect(function(err) {
+    console.log("Connected!");  
+    var sql = `UPDATE users SET name=${req.body.name}, birthday=${req.body.birthday}, address=${req.body.address}, status=${req.body.status},wife_id=${req.body.wife_id}, id_photo=${req.body.ID_photo},email=${req.body.email}, net=${req.body.net}, telephone=${req.body.telephone}, add_no=${req.body.add_no}, iptv=${req.body.iptv}, phone_no=${req.body.phone_no}, date_added=${date}, updated=${req.body.updated} WHERE id=1`;
+    connection.query(sql, function (err, result) {
+      console.log("User updated");  
+      }); 
+  }); 
+});
+app.delete('/delete_user',function(req,res){
+  connection.connect(function(err) {
+    console.log("Connected!");  
+    var sql = `DELETE FROM users WHERE id=${req.body.id}`;
+    connection.query(sql, function (err, result) {
+      console.log("User updated");  
+      }); 
+  }); 
+});
+var server = app.listen('5000', function () {
+   var port = server.address().port
+   console.log("Example app listening at", port)
+});
---
+++ backend/test.js
@@ -0,0 +1,89 @@
+/* document.getElementById('btn').addEventListener('click', () => {
+  location.replace("http://www.w3schools.com");
+
+}) */
+
+/* fetch("http://localhost:5000/register", {
+    credentials: "same-origin",
+    mode: "same-origin",
+    method: "post",
+    headers: { "Content-Type": "application/json" },
+    body: dataToSend
+})
+    .then(res => {
+        if (res.status === 200) {
+            return res.json()
+        } else {
+            console.log("Status: " + resp.status)
+            return Promise.reject("server")
+        }
+    })
+    .then(dataJson => )
+    .catch(err => {
+        if (err === "server") return
+        console.log(err)
+    })
+
+fetch('http://localhost:5000/register',)
+    .then(response => response.json())
+    .then(data => console.log(data))
+ */
+function checkbox(data){
+    if(data == 'on'){
+      return true;
+    }
+    return false;
+  };
+
+
+function get_info(){
+    telephone = document.getElementById('telephone').value;
+    telephone = checkbox(telephone);
+    add_no = document.getElementById('add_no').value;
+    add_no = checkbox(add_no);
+    iptv = document.getElementById('iptv').value;
+    iptv =  checkbox(iptv);
+    var jsfile={
+        'name' : document.getElementById("name").value,
+        'birthday' : document.getElementById('birthday').value,
+        'address' : document.getElementById('address').value,
+        'id_no' : document.getElementById('id').value,
+        'status' : document.getElementById('status').value,
+        'wife_id' : document.getElementById('wife_id').value,
+        'ID_photo' : document.getElementById('ID_photo').value,
+        'email' : document.getElementById('email').value,
+        'construction_type' : document.getElementById('construction_type').value,
+        'net' : document.getElementById('net').value,
+        'telephone': telephone,
+        'add_no':add_no,
+        'iptv':iptv,
+        'phone_no' : document.getElementById('phone_no').value,
+        'updated' : document.getElementById('updated').value,
+    }
+    var json_str = JSON.stringify(jsfile);
+    var json = JSON.parse(json_str);
+    return json;
+};
+
+function func(){
+    console.log('HELLO');
+};
+
+// document.getElementById ("btn").addEventListener ("click", get_info);
+const button = document.getElementById('btn');
+
+button.addEventListener('click', async _ => {
+  try {     
+    const response = await fetch('http://localhost:5000', {
+      method: 'post',
+      body: get_info()
+    });
+    console.log('Completed!', response);
+  } catch(err) {
+    console.error(`Error: ${err}`);
+    /* var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance
+    xmlhttp.open("POST", "http://localhost:5000/");
+    xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xmlhttp.send(get_info); */
+
+    })
