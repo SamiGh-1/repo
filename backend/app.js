3fe1d144ab72e8f500b406d6abb7df09453a8d87
Sami <68248095+SamiGh-1@users.noreply.github.com>
2022-06-13 10:00:36 +0300

    Add files via upload

--- backend/app.js
+++ backend/app.js
@@ -31,7 +31,7 @@ app.use(parser .json());
 app.use(urlencodedParser)
 
 app.post('/register',function(req,res){
-  res.send('done');
+  res.send('Add User');
   connection.connect(function(err) {
     console.log("Connected!");  
     var sql = `INSERT INTO users (name,birthday,address,id,status,wife_id,id_photo,email,net,telephone,add_no,iptv,phone_no,date_added,updated) VALUES (${req.body.name},${req.body.birthday},${req.body.address},${req.body.id},${req.body.status},${req.body.wife_id},${req.body.ID_photo},${req.body.email},${req.body.net},${req.body.telephone},${req.body.add_no},${req.body.iptv},${req.body.phone_no},${date},${req.body.updated})`;
@@ -42,20 +42,22 @@ app.post('/register',function(req,res){
 });
 
 app.put('/update_user',function(req,res){
+  res.send('Update User');
   connection.connect(function(err) {
     console.log("Connected!");  
-    var sql = `UPDATE users SET name=${req.body.name}, birthday=${req.body.birthday}, address=${req.body.address}, status=${req.body.status},wife_id=${req.body.wife_id}, id_photo=${req.body.ID_photo},email=${req.body.email}, net=${req.body.net}, telephone=${req.body.telephone}, add_no=${req.body.add_no}, iptv=${req.body.iptv}, phone_no=${req.body.phone_no}, date_added=${date}, updated=${req.body.updated} WHERE id=1`;
+    var sql = `UPDATE users SET name=${req.body.name}, birthday=${req.body.birthday}, address=${req.body.address}, id=${req.body.id}, status=${req.body.status},wife_id=${req.body.wife_id}, id_photo=${req.body.ID_photo},email=${req.body.email}, net=${req.body.net}, telephone=${req.body.telephone}, add_no=${req.body.add_no}, iptv=${req.body.iptv}, phone_no=${req.body.phone_no}, date_added=${date}, updated=${req.body.updated} WHERE id=1`;
     connection.query(sql, function (err, result) {
       console.log("User updated");  
       }); 
   }); 
 });
 app.delete('/delete_user',function(req,res){
+  res.send('Delete User');
   connection.connect(function(err) {
     console.log("Connected!");  
     var sql = `DELETE FROM users WHERE id=${req.body.id}`;
     connection.query(sql, function (err, result) {
-      console.log("User updated");  
+      console.log("User deleted");  
       }); 
   }); 
 });
